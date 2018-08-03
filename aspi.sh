daeb42acc24c495d940e295b91c6790a4682ca07
Olivier Cloux <olivier.cloux@epfl.ch>
2018-08-03 12:16:26 +0200

    modifié aspi

--- aspi.sh
+++ aspi.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-#Petit script pour juste aspirer un site web pour faire des tests en local
+#Petit script pour juste aspirer que les .html d'un site web pour faire des tests en local
 #ATTENTION: ça été fait pour une structure perso !
 #faudra modifier le script pour d'autres structures
-#zf170713.1115
+#zf180803.1156
+
 #source: https://stackoverflow.com/questions/22614331/authenticate-on-wordpress-with-wget
 
 
@@ -15,57 +16,33 @@ fi
 
 echo ---------- start aspi.sh
 
-server="test-web-wordpress.epfl.ch"
-site_name=$1
-url="/v1-testwp/"$1
-site="http://"$server$url
-login_address="$site/wp-login.php"
-log=$2
-pwd=$3
-cookies="cookies.txt"
+site=$1
 agent="Mozilla/5.0"
 
 #source ../aspi.credentials.sh
 
 echo $1
-echo $2
-echo $3
-echo ""
-echo $server$url
-echo $server$url".html"
-echo ""
 echo $site
-echo $login_address
-echo $log
-echo $pwd
 
 #exit
 
-rm -R $cookies $server$url $server$url".html"
+#rm -R $cookies $server$url $server$url".html"
 
+mkdir html
+cd html
 
-# authenticate and save cookies
 wget \
     --user-agent="$agent" \
-    --save-cookies $cookies \
-    --keep-session-cookies \
-    --delete-after \
-    --post-data="log=$log&pwd=$pwd&testcookie=1" \
-    "$login_address"
+    -E -m -e robots=off –w 10 --no-parent -A "*.html" "$site"
 
-# access home page with authenticated cookies
-wget \
-    --user-agent="$agent" \
-    --load-cookies $cookies \
-    -p -k -E -m -e robots=off –w 2 --no-parent "$site"
+#    -p -k -E -m -e robots=off –w 2 --no-parent "$site"
 
-rm $cookies
 
-echo -e "
-il y a comme nombre de pages HTML:
-"
+#echo -e "
+#il y a comme nombre de pages HTML:
+#"
 
-find $server |grep '\.html' |wc
+#find $site |grep '\.html' |wc
 
 
 #wget http://portesouvertes.epfl.ch/templates/epfl/static_epfl_menus/header_en.jsp -P ./portesouvertes.epfl.ch/templates/epfl/static_epfl_menus/

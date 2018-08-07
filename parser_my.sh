0fc6a384e9e28223f01ca2f0f8652aadd490d973
Christian Zufferey <christian@zufferey.com>
2018-08-07 08:50:17 +0200

    - ajouté le test pour ne parser seulement que les pages qui contiennent myEpflGallery

--- parser_my.sh
+++ parser_my.sh
@@ -1,39 +1,20 @@
 #!/usr/bin/env bash
 #Petit script pour parser toutes les pages de Jahia à la recherche de documents 'my'
-#zf180806.1618
-
-#echo -e " 
-#
-#Cela va prendre un certain temps, il serait bien de le faire tourner dans un 'screen' avec:
-#screen -S my        pour entrer dans screen
-#./parser_my.sh       pour lancer le serveur WEB dans screen
-#CTRL+a,d                pour sortir de screen en laissant tourner le serveur
-#screen -r my        pour revenir dans screen
-#CTRL+d                  pour terminer screen
-#screen -list            pour lister tous les screen en fonctionement
-#"
-#read -p "appuyer une touche pour démarrer parser_my.sh"
+#zf180807.0849
 
 zcsv=`cat liste_pages.csv`
 
-for i in $zcsv ; do 
-#    echo -e $i
-    while read zline; do 
-#        echo $zline
-
-        if [[ $zline == *"documents.epfl.ch"* ]]; then
-#echo "yyy"
-            echo $zline | sed 's/"/\n/g' | grep "documents.epfl.ch" | egrep -i ".jpg|.png|.gif|.pdf" > temp.csv
-#            cat temp.csv
-
-            ztemp=`cat temp.csv`
-            for j in $ztemp ; do
-                echo -e $i", "$j
-            done
-
-        fi
-
-    done < $i
-done
-
+if grep -lq myEpflGalleryBox $zcsv ; then
+    for i in $zcsv ; do
+        while read zline; do
+            if [[ $zline == *"documents.epfl.ch"* ]]; then
+                echo $zline | sed 's/"/\n/g' | grep "documents.epfl.ch" | egrep -i ".jpg|.png|.gif|.pdf" > temp.csv
+                ztemp=`cat temp.csv`
+                for j in $ztemp ; do
+                    echo -e $i", "$j
+                done
+            fi
+        done < $i
+    done
+fi
 

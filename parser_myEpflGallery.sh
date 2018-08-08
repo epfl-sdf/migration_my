20befe1a8ffba2d180a500f0e95ad81f445f45d1
Christian Zufferey <christian@zufferey.com>
2018-08-08 18:02:35 +0200

    wip

---
+++ parser_myEpflGallery.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#Petit script pour parser toutes les pages d'un site à la recherche de documents de boîtes myEpflGallery pour voir si un site a bien été migré
+#zf180808.1758
+
+
+faut en premier aspirer le site avec aspi.sh
+puis le parser avec ce script modifié pour 
+
+
+
+zcsv=`cat liste_pages.csv`
+echo "pages" > liste_pages_myEpflGallery.csv
+
+for i in $zcsv ; do
+    if grep -lq myEpflGalleryBox $i ; then
+        echo $i >> liste_pages_myEpflGallery.csv
+        while read zline; do
+            if [[ $zline == *"documents.epfl.ch"* ]]; then
+                echo $zline | sed 's/"/\n/g' | grep "documents.epfl.ch" > temp.csv
+                ztemp=`cat temp.csv`
+                for j in $ztemp ; do
+                    if [[ $j == *".jpg"* ]] || [[ $j == *".JPG"* ]] || [[ $j == *".png"* ]] || [[ $j == *".gif"* ]] || [[ $j == *".pdf"* ]]  || [[ $j == *".doc"* ]]; then
+                        echo -e $i","$j
+                    fi
+                done
+            fi
+        done < $i
+    fi
+done
+
